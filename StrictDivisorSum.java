89469ba885c891807f8454f0885de4359aeceae2
Jesemeil <134516274+Jesemeil@users.noreply.github.com>
2023-06-22 11:38:02 +0100

    Mastering Concepts

---
+++ StrictDivisorSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StrictDivisorSum {
+        public static void main(String[] args) {
+                Scanner s = new Scanner(System.in);
+
+                System.out.print("Enter an integer: ");
+                int n = s.nextInt();
+
+                int sum = 0;
+                for (int i= 1; i <= n / 2; i++) {
+                        if (n % i == 0)
+                                sum += i;
+
+                }
+                System.out.println("Sum of strict divisors: " + sum );
+        }
+}
